f89fe75c2da4a050b519fa20537588b40270a98b
samaniego.287741 <samaniego.287741@LAB-A-PC19>
2024-02-08 08:02:05 +0800

    Add project files.

---
+++ GreeetingApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GreeetingApp
+{
+    internal class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the total Number of your courses: ");
+            int course = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book: ");
+            double BookPrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Your Name: {name}");
+            Console.WriteLine($"Total Enroll Courses: {course}");
+            Console.WriteLine($"Price of my fovorite Book: {BookPrice} ");
+
+            Console.Write("Press Any Key to Exit....");
+            Console.ReadKey();
+        }
+    }
+}
