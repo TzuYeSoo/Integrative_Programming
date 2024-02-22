531579ef4a428707c081d025f845a194efd3d1eb
Jessie D. Samaniego <150759974+TzuYeSoo@users.noreply.github.com>
2024-02-23 07:35:30 +0800

    Add files via upload

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static double studentAve, totalgrades, roundGrades;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter 5 grades separated by new line");
+            for(int i = 0; i < 5;  i++) 
+            {
+                
+                double grades = Convert.ToDouble(Console.ReadLine());
+
+                totalgrades += grades;
+
+            }
+
+            studentAve = totalgrades / 5;
+            roundGrades = Math.Round(studentAve);
+
+            Console.WriteLine("The Averages is " +  studentAve + " and the round of is " + roundGrades);
+
+            Console.WriteLine("Enter any key to exit...");
+            Console.ReadKey(); 
+        }
+    }
+}
