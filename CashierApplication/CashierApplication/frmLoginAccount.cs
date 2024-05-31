6d308fb94697809a2fdeb54d7015cc28cba770d5
Jessie D. Samaniego <150759974+TzuYeSoo@users.noreply.github.com>
2024-05-31 09:46:29 +0800

    Add files via upload

---
+++ CashierApplication/CashierApplication/frmLoginAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashierApplication
+{
+    public partial class frmLoginAccount : Form
+    {
+        public frmLoginAccount()
+        {
+            InitializeComponent();
+        }
+        static double price, discount, quantity;
+
+
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+           // if (Login.Text.Equals("Jessie") && pass.Text.Equals("Pogi"))
+           // {
+                frmPurchasedDiscountedItem frmp = new frmPurchasedDiscountedItem();
+                frmp.Show();
+                this.Hide();
+            //}
+           // else
+           // {
+                MessageBox.Show("Invalid Inputs");
+           // }
+        }
+    }
+}
